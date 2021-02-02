573823b78f4f516140f23e6c6e79e11286bdeb96
Debian <debian@debian.debian.me>
2021-02-01 23:52:06 -0300

    update

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from .models import Post
 
 class PostView(TemplateView):
-	template_name = 'blog/post.html'
+	template_name = 'blog/single.html'
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
@@ -14,6 +14,6 @@ class PostView(TemplateView):
 			if not Post.objects.filter(id=kwargs['post_id']).exists():
 				raise Http404
 			else:
-				context['post'] = Post.objects.get(id=post_id)
+				context['post'] = Post.objects.get(id=kwargs['post_id'])
 
 		return context
